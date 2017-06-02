b20cf7d04abd605278511d78abe368f32dc22a33
Mikko Matilainen <mikko.matilainen@gofore.com>
2017-06-02 15:03:38 +0300

    Simple bus lambda

--- handler.js
+++ handler.js
@@ -1,15 +1,44 @@
 'use strict';
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+const fetch = require('node-fetch');
 
-  callback(null, response);
+const getEssentialInfo = res => res.body.map(({ monitoredVehicleJourney }) => {
+    const {
+      lineRef,
+      vehicleLocation: { longitude, latitude },
+      bearing,
+      delay
+    } = monitoredVehicleJourney;
+
+    return {
+      lineRef,
+      longitude,
+      latitude,
+      bearing,
+      delay
+    }
+  });
+
+const fetchData = (callback) => {
+  const createResponse = body => ({
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+    },
+    body: JSON.stringify(body)
+  });
+
+  const url = 'http://data.itsfactory.fi/journeys/api/1/vehicle-activity?lineRef=5';
+  fetch(url)
+    .then(res => res.json())
+    .then(getEssentialInfo)
+    .then(json => callback(null, createResponse(json)))
+}
+
+module.exports.hello = (event, context, callback) => {
+  fetchData(callback);
+
+  // callback(null, response);
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
